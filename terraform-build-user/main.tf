a02b5bafcbeffa4cecbb100db7e3fde538b18658
Dave Redmin <david.redmin@beta.dhs.gov>
2020-09-28 16:19:42 -0400

    Rename repository references after clone.

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -10,9 +10,9 @@ module "iam_user" {
   }
 
   ssm_parameters = ["/cyhy/dev/users", "/ssh/public_keys/*"]
-  user_name      = "build-skeleton-packer"
+  user_name      = "build-docker-packer"
   tags = {
     Team        = "CISA - Development"
-    Application = "skeleton-packer"
+    Application = "docker-packer"
   }
 }
